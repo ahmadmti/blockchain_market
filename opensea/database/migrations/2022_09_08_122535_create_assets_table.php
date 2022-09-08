046be936e49d8ba438253bf8e928e09bb64c7f0d
Awais Javaid <53362912+awaisjavaid930@users.noreply.github.com>
2022-09-08 17:55:36 +0500

    Commentation Setting
    For the Commentation

--- opensea/database/migrations/2022_09_08_122535_create_assets_table.php
+++ opensea/database/migrations/2022_09_08_122535_create_assets_table.php
@@ -16,13 +16,13 @@ return new class extends Migration
         Schema::create('assets', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('link')->nullable();                 //Exteranl Link
+            $table->string('link')->nullable();                     //Exteranl Link
             $table->text('description')->nullable();        
             $table->integer('collection_id');     
-            $table->string('hidden_content')->nullable();       // Trigger
-            $table->boolean('sensitive_content');                  // Porngraphic Content     
-            $table->integer('supply');                          // Number of Item Minted
-            $table->string('metadata'); // Metadata about is it upload on blockchain or hash
+            $table->string('hidden_content')->nullable();           // Trigger
+            $table->boolean('sensitive_content')->default('false'); // Porngraphic Content     
+            $table->integer('supply');                              // Number of Item Minted
+            $table->string('metadata')->nullable(); // Store Media on IPFS hash or nullabe
             $table->timestamps();
         });
     }
