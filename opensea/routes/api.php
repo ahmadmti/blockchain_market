529c78c58dc074c9607378e07182da518bd61db8
Awais Javaid <53362912+awaisjavaid930@users.noreply.github.com>
2022-09-09 10:07:02 +0500

    Install Packages for Web3Login Add Routes and Login Create Function
    Install Packages for Web3Login Add Apis Routes and Login Create Function

--- opensea/routes/api.php
+++ opensea/routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('/web3-login-message', 'Web3LoginController@message');
+Route::post('/web3-login-verify', 'Web3LoginController@verify');
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
